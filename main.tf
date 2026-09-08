5f8bd4ac5ee585641a94ec587746b016b7fbaf32
venugh8899 <venugk8899@gmail.com>
2026-09-08 20:12:47 +0530

    Change AWS region and add S3 bucket resource
    Updated AWS region and added an S3 bucket resource.

--- main.tf
+++ main.tf
@@ -1,20 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-035827357e3c7e810"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Terraform-Jenkins-Demo"
-  }
+resource "aws_s3_bucket" "demo" {
+  bucket = "terraform-jenkins-demo-123456789"
 }
