425b72bf92edbf5f1142da11cc3d63efe20aa3df
venugh8899 <venugk8899@gmail.com>
2026-09-08 20:22:10 +0530

    Update main.tf to use variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "terraform-jenkins-demo-123456789"
+  bucket = var.bucket_name
 }
