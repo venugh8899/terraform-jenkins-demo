fa03fc39f14e4afaf4fa9e2511b56aceb66c6a3b
venugh8899 <venugk8899@gmail.com>
2026-09-08 20:17:42 +0530

    Add Terraform variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
